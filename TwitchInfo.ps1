061e81502f5f1c22595e82f6a7a48034456b62a7
VooDoo <dearvoodoo.yt@gmail.com>
2023-04-29 02:48:45 +0200

    Change path
    Change path to be compatible with everyone.
    The script now get the script path and then check for data folder.
    
    Changing some font lines:
    Change default font to Segoe UI
    You can customize your font in the file options.txt

--- TwitchInfo.ps1
+++ TwitchInfo.ps1
@@ -1,12 +1,14 @@
-$path_to_file = "data/info.txt"
+$path_to_file = Get-ChildItem $PSScriptRoot\data\info.txt
 
 # Get the  list of categories via github raw. So when I update it you don't have to update on your side :)
 $source = 'https://raw.githubusercontent.com/dearvoodoo/TP-stream_info/main/data/categories.txt'
-$path_to_categories = "data/categories.txt"
+$path_to_categories = Get-ChildItem $PSScriptRoot\data\categories.txt
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($source, $path_to_categories)
 
-$path_to_options = "data/options.txt"
+$path_to_options = Get-ChildItem $PSScriptRoot\data\options.txt
+
+$ico = Get-ChildItem $PSScriptRoot\data\twitch.ico
 
 if (-not(Test-Path $path_to_categories)) {
     "Apex Legends", "Elden Ring", "Fortnite", "Grand Theft Auto V", "Just Chatting", "League Of Legends", "Minecraft", "Overwatch 2", "Sons of the Forest", "Valorant", "Wobbly Life" | Set-Content $path_to_categories -Encoding utf8
@@ -22,8 +24,13 @@ $options = Get-Content $path_to_options
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+$font_name = $options[5]
+$font_bold_8 = [System.Drawing.Font]::new($font_name, 8, [System.Drawing.FontStyle]::Bold)
+$font_bold_12 = [System.Drawing.Font]::new($font_name, 12, [System.Drawing.FontStyle]::Bold)
+$font_10 = [System.Drawing.Font]::new($font_name, 10)
+
 $form = New-Object System.Windows.Forms.Form
-$form.Icon = New-Object System.Drawing.Icon "data/twitch.ico"
+$form.Icon = New-Object System.Drawing.Icon $ico
 $form.Text = $options[2]
 $form.Size = New-Object System.Drawing.Size(440,310)
 $form.BackColor = [System.Drawing.Color]::FromArgb(16, 16, 16)
@@ -37,7 +44,7 @@ $okButton.Location = New-Object System.Drawing.Point(80,210)
 $okButton.Size = New-Object System.Drawing.Size(75,23)
 $okButton.BackColor = [System.Drawing.Color]::FromArgb(101, 98, 251)
 $okButton.ForeColor = [System.Drawing.Color]::White
-$okButton.Font = [System.Drawing.Font]::new("Arial", 8, [System.Drawing.FontStyle]::Bold)
+$okButton.Font = $font_bold_8
 $okButton.Text = $options[1]
 $okButton.FlatStyle = 'Flat'
 $okButton.FlatAppearance.BorderSize = 0
@@ -53,7 +60,7 @@ $cancelButton.Location = New-Object System.Drawing.Point(270,210)
 $cancelButton.Size = New-Object System.Drawing.Size(75,23)
 $cancelButton.BackColor = [System.Drawing.Color]::FromArgb(41, 45, 51)
 $cancelButton.ForeColor = [System.Drawing.Color]::White
-$cancelButton.Font = [System.Drawing.Font]::new("Arial", 8, [System.Drawing.FontStyle]::Bold)
+$cancelButton.Font = $font_bold_8
 $cancelButton.Text = $options[0]
 $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
 $cancelButton.FlatStyle = 'Flat'
@@ -68,7 +75,7 @@ $label = New-Object System.Windows.Forms.Label
 $label.Location = New-Object System.Drawing.Point(40,20)
 $label.Size = New-Object System.Drawing.Size(280,20)
 $label.Text = $options[4]
-$label.Font = [System.Drawing.Font]::new("Arial", 12, [System.Drawing.FontStyle]::Bold)
+$label.Font = $font_bold_12
 $label.ForeColor = [System.Drawing.Color]::White
 $form.Controls.Add($label)
 
@@ -79,7 +86,7 @@ $textArea.BorderStyle = "None" # Add this line to remove the border
 $textArea.Location = New-Object System.Drawing.Point(40,45)
 $textArea.Size = New-Object System.Drawing.Size(345,75)
 $textArea.BackColor = [System.Drawing.Color]::FromArgb(61, 61, 64)
-$textArea.Font = [System.Drawing.Font]::new("Arial", 10)
+$textArea.Font = $font_10
 $textArea.ForeColor = [System.Drawing.Color]::White
 $textArea.BorderStyle = [System.Windows.Forms.BorderStyle]::None # Add this line to set the flat style
 
@@ -95,7 +102,7 @@ $label = New-Object System.Windows.Forms.Label
 $label.Location = New-Object System.Drawing.Point(40,135)
 $label.Size = New-Object System.Drawing.Size(280,20)
 $label.Text = $options[3]
-$label.Font = [System.Drawing.Font]::new("Arial", 12, [System.Drawing.FontStyle]::Bold)
+$label.Font = [System.Drawing.Font]::new("Ariel", 12, [System.Drawing.FontStyle]::Bold)
 $label.ForeColor = [System.Drawing.Color]::White
 $form.Controls.Add($label)
 
@@ -104,7 +111,7 @@ $dropDown.Location = New-Object System.Drawing.Point(40, 160)
 $dropDown.Size = New-Object System.Drawing.Size(345, 21)
 $dropDown.DropDownStyle = "DropDown"
 $dropDown.BackColor = [System.Drawing.Color]::FromArgb(61, 61, 64)
-$dropDown.Font = [System.Drawing.Font]::new("Arial", 10)
+$dropDown.Font = [System.Drawing.Font]::new("Ariel", 10)
 $dropDown.ForeColor = [System.Drawing.Color]::White
 $dropDown.FlatStyle = 'Flat'
 $category = Get-Content $path_to_categories
